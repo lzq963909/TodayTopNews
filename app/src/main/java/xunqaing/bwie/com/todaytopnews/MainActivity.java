8d8caaab96e0a49eb0cf636ea150712ea0421ef1
NarukoSama <981692040@qq.com>
2017-05-08 18:34:18 +0800

    。

--- app/src/main/java/xunqaing/bwie/com/todaytopnews/MainActivity.java
+++ app/src/main/java/xunqaing/bwie/com/todaytopnews/MainActivity.java
@@ -10,8 +10,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //传的人最傻逼...
-        //...
-        //
+
+        //。。。。。。。。。。
     }
 }
