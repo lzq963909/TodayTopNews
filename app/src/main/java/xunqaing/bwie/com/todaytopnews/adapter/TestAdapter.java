cc2b467e5fb3261c1d0bca51c234105c6c4562d5
Terdimire <867133009@qq.com>
2017-05-08 19:29:13 +0800

    Before Do SomeThing Parpared

---
+++ app/src/main/java/xunqaing/bwie/com/todaytopnews/adapter/TestAdapter.java
@@ -0,0 +1,8 @@
+package xunqaing.bwie.com.todaytopnews.adapter;
+
+/**
+ * Created by Administrator on 2017/5/8 0008.
+ */
+
+public class TestAdapter {
+}
---
+++ app/src/main/java/xunqaing/bwie/com/todaytopnews/fragment/TestFragment.java
@@ -0,0 +1,8 @@
+package xunqaing.bwie.com.todaytopnews.fragment;
+
+/**
+ * Created by Administrator on 2017/5/8 0008.
+ */
+
+public class TestFragment {
+}
