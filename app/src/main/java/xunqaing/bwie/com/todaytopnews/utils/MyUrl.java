ae567b775eb1e9127a90028e274487c6196a62fc
Terdimire <867133009@qq.com>
2017-05-09 18:01:10 +0800

    Myurl 地址修改

--- app/src/main/java/xunqaing/bwie/com/todaytopnews/utils/MyUrl.java
+++ app/src/main/java/xunqaing/bwie/com/todaytopnews/utils/MyUrl.java
@@ -2,50 +2,38 @@ package xunqaing.bwie.com.todaytopnews.utils;
 
 import java.util.Date;
 
-/** * 
- * @author  ����Trydimire E-mail: 867133009@qq.com
- * @date ����ʱ�䣺2015-8-7 ����11:31:52 
- * @version 1.0 
- * @parameter 
- * @since 
+/** *
+ * @author  作者Trydimire E-mail: 867133009@qq.com
+ * @date 创建时间：2015-8-7 上午11:31:52
+ * @version 1.0
+ * @parameter
+ * @since
  * @return  */
 public class MyUrl {
-	
-	/**�ȴʵĽӿڵ�ַ **/
+
+	/**热词的接口地址 **/
 	public static final String HOT_WORDS_URL= "http://toutiao.com/hot_words/?_=";
-	/**�Ƽ����� �ĵ�ַ **/
-	public static final String ALL_NEWS_URL= "http://toutiao.com/api/article/recent/?source=2&category=__all__&max_behot_time=0&_=";
-	/**�ȵ����� �ĵ�ַ **/
-	public static final String HOT_NEWS_URL= "http://toutiao.com/api/article/recent/?source=2&category=news_hot&max_behot_time=0&_=";
-	/**������ŵĵ�ַ **/
-	public static final String SOCIETY_NEWS_URL= "http://toutiao.com/api/article/recent/?source=2&category=news_society&max_behot_time=0&_=";
-	/**�������ŵĽӿڵ�ַ **/
-	public static final String ENTERTAINMENT_NEWS_URL= "http://toutiao.com/api/article/recent/?source=2&category=news_entertainment&max_behot_time=0&_=";
-	/**�Ƽ����ŵĽӿڵ�ַ **/
-	public static final String TECH_NEWS_URL= "http://toutiao.com/api/article/recent/?source=2&category=news_car&max_behot_time=0&_=";
-	/**�������ŵĽӿڵ�ַ**/
-	public static final String NEWS_SPORTS = "http://toutiao.com/api/article/recent/?source=2&category=news_sports&max_behot_time=0&_=";
-	
-	/**����ѡ��Ľӿڵ�ַ**/
+
+	/**新闻选项的接口地址**/
 	public static final String NEWS_CATEGORY = "http://ic.snssdk.com/article/category/get/v2/?iid=2939228904";
-	/**���г��еĽӿڵ�ַ(����ĸ��ʽ����)**/
+
+	/**所有城市的接口地址(按字母方式排列)**/
 	public static final String CITY_ADDRESS = "http://ic.snssdk.com/2/article/city/";
-	/**�����м������µĽӿ�**/
-	public static final String UPDATE_COUNTS="http://ic.snssdk.com/2/article/v27/refresh_tip/?min_behot_time=1439292102&iid=2939228904";
-	
-	private static  String urlone = "http://toutiao.com/api/article/recent/?source=2&category=";
-	private static String urltwo = "&max_behot_time=0&_=";
-	
+
+	/**弹出有几条更新的接口**/
+	public static final String UPDATE_COUNTS="http://ic.snssdk.com/2/article/v27";
+	/**拼接的第一个参数**/
+	private static  String urlone = "http://ic.snssdk.com/2/article/v25/stream/?category=";
 	/**
-	 * ����Ƶ�������ؽӿڵ�ַ
-	 * �÷���ƴ�ӿ��Ի�ȡʵʱ����
-	 * newstype ������ѡ��Ľӿ��л��
+	 * 传入频道名返回接口地址
+	 * 该方法拼接可以获取实时新闻
+	 * newstype 从新闻选项的接口中获得 新闻接口中的 ategory中的数据
 	 * */
 	public   static String getUrl(String newstype) {
 		String url = "";
-		Date dt= new Date();
-		Long time= dt.getTime();
-		url = urlone + newstype + urltwo + time;
+
+		url = urlone + newstype;
 		return url;
 	}
+
 }
