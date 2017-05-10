11747e92a161e7bb64fe51853d9d4607ee62cd0a
NarukoSama <981692040@qq.com>
2017-05-10 14:59:09 +0800

    日间模式和夜间模式的开头

---
+++ app/src/main/java/xunqaing/bwie/com/todaytopnews/SwitchButtonEvent.java
@@ -0,0 +1,29 @@
+package xunqaing.bwie.com.todaytopnews;
+
+/**
+ * Created by
+ * Chenxin
+ * on 2017/5/10.
+ */
+
+public class SwitchButtonEvent {
+
+
+    //true 代表白天模式  false为夜间
+    private boolean isWhite;
+
+    public SwitchButtonEvent() {
+    }
+
+    public SwitchButtonEvent(boolean isWhite) {
+        this.isWhite = isWhite;
+    }
+
+    public boolean isWhite() {
+        return isWhite;
+    }
+
+    public void setWhite(boolean white) {
+        isWhite = white;
+    }
+}
--- app/src/main/java/xunqaing/bwie/com/todaytopnews/fragment/MenuRightFragment.java
+++ app/src/main/java/xunqaing/bwie/com/todaytopnews/fragment/MenuRightFragment.java
@@ -7,8 +7,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 
+import com.kyleduo.switchbutton.SwitchButton;
+
+import org.greenrobot.eventbus.EventBus;
+
+import xunqaing.bwie.com.todaytopnews.Constants.Constants;
 import xunqaing.bwie.com.todaytopnews.R;
+import xunqaing.bwie.com.todaytopnews.SwitchButtonEvent;
+import xunqaing.bwie.com.todaytopnews.utils.PreferencesUtils;
 
 /**
  * Created by : Xunqiang
@@ -17,14 +25,32 @@ import xunqaing.bwie.com.todaytopnews.R;
 
 public class MenuRightFragment extends Fragment {
 
+    private SwitchButton switchButton;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.menuright_fragment,container,false);
 
+        switchButton = (SwitchButton) view.findViewById(R.id.switch_btn);
+
         return view;
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
 
+        switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+
+                boolean mode = PreferencesUtils.getValueByKey(getContext(), Constants.isNightMode,isChecked);
+
+                EventBus.getDefault().post(new SwitchButtonEvent(mode));
+
+            }
+        });
+    }
 }
