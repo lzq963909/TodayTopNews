10388935dd514e616ae2b2a64d2a495fb0a6173e
huanhuan1234 <905136363@qq.com>
2017-05-10 14:43:14 +0800

    微博登录布局

--- app/src/main/java/xunqaing/bwie/com/todaytopnews/adapter/NewsListAdapter.java
+++ app/src/main/java/xunqaing/bwie/com/todaytopnews/adapter/NewsListAdapter.java
@@ -1,16 +1,14 @@
 package xunqaing.bwie.com.todaytopnews.adapter;
 
-import java.util.List;
-
-
 import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.TextView;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
+
+import java.util.List;
 
 import xunqaing.bwie.com.todaytopnews.R;
 import xunqaing.bwie.com.todaytopnews.bean.TuijianBean;
@@ -20,10 +18,11 @@ public class NewsListAdapter extends BaseAdapter{
 	Activity activity;
 	LayoutInflater inflater = null;
 	ImageLoader imageLoader = ImageLoader.getInstance();
+
 	public NewsListAdapter(List<TuijianBean.DataBean> newsList) {
 		this.newsList = newsList;
 	}
-	
+
 
 
 	@Override
@@ -49,6 +48,7 @@ public class NewsListAdapter extends BaseAdapter{
 		return position;
 	}
 
+
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		// TODO Auto-generated method stub
@@ -56,6 +56,7 @@ public class NewsListAdapter extends BaseAdapter{
 		View view = convertView;
 		if (view == null) {
 			view = inflater.inflate(R.layout.list_item, null);
+
 			mHolder = new ViewHolder();
 			view.setTag(mHolder);
 		} else {
