eb8f4b707be8bedb71febef3d790c17316922148
NarukoSama <981692040@qq.com>
2017-05-10 15:02:32 +0800

    日间模式和夜间模式的开头

--- app/src/main/java/xunqaing/bwie/com/todaytopnews/fragment/MenuRightFragment.java
+++ app/src/main/java/xunqaing/bwie/com/todaytopnews/fragment/MenuRightFragment.java
@@ -7,16 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CompoundButton;
 
-import com.kyleduo.switchbutton.SwitchButton;
-
-import org.greenrobot.eventbus.EventBus;
-
-import xunqaing.bwie.com.todaytopnews.Constants.Constants;
 import xunqaing.bwie.com.todaytopnews.R;
-import xunqaing.bwie.com.todaytopnews.SwitchButtonEvent;
-import xunqaing.bwie.com.todaytopnews.utils.PreferencesUtils;
 
 /**
  * Created by : Xunqiang
@@ -25,7 +17,7 @@ import xunqaing.bwie.com.todaytopnews.utils.PreferencesUtils;
 
 public class MenuRightFragment extends Fragment {
 
-    private SwitchButton switchButton;
+
 
     @Nullable
     @Override
@@ -33,24 +25,16 @@ public class MenuRightFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.menuright_fragment,container,false);
 
-        switchButton = (SwitchButton) view.findViewById(R.id.switch_btn);
+
 
         return view;
     }
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
 
-                boolean mode = PreferencesUtils.getValueByKey(getContext(), Constants.isNightMode,isChecked);
-
-                EventBus.getDefault().post(new SwitchButtonEvent(mode));
-
-            }
-        });
     }
 }
