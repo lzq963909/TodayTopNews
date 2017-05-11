fd003b938a55a57db99cc9d03bc8d4645ff6f86b
huanhuan1234 <905136363@qq.com>
2017-05-11 08:54:00 +0800

    Merge branch 'master' of https://github.com/sxnq/TodayTopNews
    # Conflicts:
    #	app/src/main/java/xunqaing/bwie/com/todaytopnews/fragment/MenuRightFragment.java
    #	app/src/main/java/xunqaing/bwie/com/todaytopnews/fragment/NewsMainFragment.java

--- app/src/main/java/xunqaing/bwie/com/todaytopnews/fragment/NewsMainFragment.java
+++ app/src/main/java/xunqaing/bwie/com/todaytopnews/fragment/NewsMainFragment.java
@@ -16,6 +16,7 @@ import org.xutils.ex.DbException;
 import org.xutils.http.RequestParams;
 import org.xutils.x;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import xunqaing.bwie.com.todaytopnews.IApplication;
@@ -34,9 +35,10 @@ import static org.xutils.x.getDb;
 public class NewsMainFragment extends Fragment {
     private ListView listView;
     private String newsType;
-    private List<TuijianBean.DataBean> list;
+    private List<TuijianBean.DataBean> list=new ArrayList<TuijianBean.DataBean>();
     private NewsListAdapter adapter;
     private IApplication application;
+
 
     @Nullable
     @Override
@@ -73,18 +75,20 @@ public class NewsMainFragment extends Fragment {
 
             @Override
             public void onSuccess(String result) {
+
                 TuijianBean tuijianBean = JSON.parseObject(result,TuijianBean.class);
-                list = tuijianBean.getData();
+
+                list.addAll(tuijianBean.getData());
+
                 adapter = new NewsListAdapter(getActivity(),list);
+
                 listView.setAdapter(adapter);
 
                 manager = getDb(application.config);
 
                 try {
-                    for (TuijianBean.DataBean dataBean: list) {
-                        manager.save(dataBean);
-                    }
 
+                    manager.save(tuijianBean.getData());
 
                 } catch (DbException e) {
                     e.printStackTrace();
