f8c81321c42473a4095162d6372d13b6972f7bcb
huanhuan1234 <905136363@qq.com>
2017-05-12 13:42:42 +0800

    set设置

---
+++ app/src/main/java/xunqaing/bwie/com/todaytopnews/activities/SetActivity.java
@@ -0,0 +1,19 @@
+package xunqaing.bwie.com.todaytopnews.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import xunqaing.bwie.com.todaytopnews.R;
+
+/**
+ * Created by Huangminghuan on 2017/5/12.
+ */
+
+public class SetActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.set);
+
+    }
+}
