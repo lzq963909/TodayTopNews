0e08862fb0f43617d3993d09df324d29aa77e767
huanhuan1234 <905136363@qq.com>
2017-05-15 11:19:39 +0800

    。。。。

--- app/src/main/java/xunqaing/bwie/com/todaytopnews/IApplication.java
+++ app/src/main/java/xunqaing/bwie/com/todaytopnews/IApplication.java
@@ -37,6 +37,7 @@ public class IApplication extends Application {
         PlatformConfig.setWeixin("wx967daebe835fbeac", "5bb696d9ccd75a38c8a0bfe0675559b3");
         PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
         PlatformConfig.setSinaWeibo("3921700954", "04b48b094faeb16683c32669824ebdad", "http://sns.whalecloud.com");
+
     }
 
     private void initgetui(IApplication iApplication) {
